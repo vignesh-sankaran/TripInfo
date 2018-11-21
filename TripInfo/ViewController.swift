b11ee4f54fc38c858d13d3dbd4dd7416c3be7afa
Vignesh Sankaran <vignesh@sankarans-mbp>
2018-11-21 16:10:41 +1100

    Initial Commit

---
+++ TripInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripInfo
+//
+//  Created by Vignesh Sankaran on 21/11/18.
+//  Copyright © 2018 Vignesh Sankaran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
