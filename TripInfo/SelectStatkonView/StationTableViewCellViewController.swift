9532c02fa6594020942b6b7b837803ed26f79464
Vignesh Sankaran <developer@sankaran.io>
2018-12-18 10:24:00 +1100

    Create prototype result cell and link to view controller

---
+++ TripInfo/SelectStatkonView/StationTableViewCellViewController.swift
@@ -0,0 +1,15 @@
+//
+//  StationTableViewCell.swift
+//  TripInfo
+//
+//  Created by Vignesh Sankaran on 14/12/18.
+//  Copyright © 2018 Vignesh Sankaran. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StationTableViewCellViewController {
+    @IBOutlet weak var stationNameLabel: UILabel!
+    @IBOutlet weak var stationLineLabel: UILabel!
+}
